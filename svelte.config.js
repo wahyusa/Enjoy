0e71ab412d2990fa4a985de523a199e1c48b8f81
Wahyu Syamsul A'lam <wahyuskull22@gmail.com>
2022-07-14 08:27:05 +0000

    Real project setup with sveltekit and tailwindcss

---
+++ svelte.config.js
@@ -0,0 +1,31 @@
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
+
+	kit: {
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+			fallback: null,
+			precompress: false
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'development' ? '' : '/revenge'
+		},
+		prerender: {
+			default: true
+		}
+	}
+};
+
+export default config;
